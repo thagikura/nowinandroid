f284ad8b075f81a2044ee00626a33e44373a9ad9
Takeshi Hagikura <thagikura@google.com>
2022-08-12 18:44:19 +0900

    WIP Add GradleManagedDevice convention plugin

---
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/GradleManagedDevice.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid
+
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.internal.dsl.ManagedVirtualDevice
+import com.android.build.gradle.internal.testing.utp.maybeCreateUtpConfigurations
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+/**
+ * Configure base Kotlin with Android options
+ */
+internal fun Project.configureGradleManagedDevice(
+    commonExtension: CommonExtension<*, *, *, *>,
+) {
+    commonExtension.apply {
+        testOptions {
+            managedDevices {
+
+                ManagedVirtualDevice("pixel4Api30").apply {
+                    device = "Pixel 4"
+                    apiLevel = 30
+                    systemImageSource = "aosp-atd"
+                    require64Bit = false
+                }
+            }
+        }
+    }
+
+}
+
